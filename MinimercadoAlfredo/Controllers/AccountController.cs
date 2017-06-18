755ce8f976b6396d94692cb3e36f81bf667c7bd4
Maurixrs <mauriciooreyes@gmail.com>
2017-06-17 23:13:37 -0300

    correción login

--- MinimercadoAlfredo/Controllers/AccountController.cs
+++ MinimercadoAlfredo/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using MinimercadoAlfredo.Models;
 
 namespace MinimercadoAlfredo.Controllers
 {
-    [Authorize(Users = "luisalfredopiriz@yahoo.com.ar")]
+   
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
